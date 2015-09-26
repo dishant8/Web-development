ce99d81081f17e2724c89c1eab4338506cedf5ca
Dishant Shah <dishant8@ccs.neu.edu>
2015-09-26 17:47:36 -0400

    testing app working locally

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 <<<<<<< HEAD
-app.use(express.static(__dirname + '/public/WebDevWorkspace/Assignment1/WebContent'));
+app.use(express.static(__dirname + '/public/WebDevWorkspace'));
 =======
 app.use(express.static(__dirname + '\WebDevWorkspace\Assignment1\WebContent'));
 >>>>>>> 8d2ee59a95e7d15a7afdc57e171a219a5d5f88be
