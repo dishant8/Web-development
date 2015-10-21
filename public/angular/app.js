5dd943a4110eaee1c2033859c434c09c5b1369f3
Dishant Shah <dishant8@ccs.neu.edu>
2015-10-21 16:47:22 -0400

    testing angular

---
+++ public/angular/app.js
@@ -0,0 +1,28 @@
+﻿(function () {
+
+    angular.module("WhiteBoardApp", []);
+
+    angular.module("WhiteBoardApp")
+    .controller("HelloWorldController", HelloWorldController);
+
+    function HelloWorldController($scope) {
+        $scope.hello = "Hello World!!!";
+        $scope.courseName = "Java101";
+
+        $scope.user = {
+            fname: "alice",
+            lname: "wonderkland"
+        };
+
+        var course = { title: "C# 101", seats: 25, starts: new Date() };
+        console.log = ("hellowworld!!");
+
+        var courses = [
+            course,
+            { title: "python", seats: 12, starts: new Date() },
+            { title: "python", seats: 12, starts: new Date() },
+        ]
+        $scope.courses = courses;
+
+    }
+})();
