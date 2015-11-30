827c33dc75e380ea938cdae2873b98a6038e195e
Dishant Shah <dishant8@ccs.neu.edu>
2015-11-30 17:38:43 -0500

    changing server.js to work on openshift

--- server.js
+++ server.js
@@ -4,9 +4,21 @@ var bodyParser = require('body-parser');
 //var q = require('q');
 
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/cs5610fall2015exmpl1');
+//mongoose.connect('mongodb://localhost/cs5610fall2015exmpl1');
 //mongoose.connect(process.env.OPENSHIFT_MONGODB_DB_URL || 'mongodb://localhost/cs5610')
-var db = mongoose.connection;
+//var db = mongoose.connection;
+
+var connectionString = 'mongodb://127.0.0.1:27017/cs5610fall2015exmpl1';
+
+if (process.env.OPENSHIFT_MONGODB_DB_PASSWORD) {
+    connectionString = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+        process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+        process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+        process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+        process.env.OPENSHIFT_APP_NAME;
+}
+
+var db = mongoose.connect(connectionString);
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
@@ -17,7 +29,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 
-require("./public/assignment/server/app.js")(app,mongoose,db);
+require("./public/assignment/server/app.js")(app, mongoose, db);
 require("./public/practice/mongo/server/app.js")(app, mongoose, db);
 
 app.listen(port, ipaddress);
